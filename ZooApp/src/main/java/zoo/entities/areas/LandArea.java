f26a1528b70a3ab9c396837f6caa4f36c5db6349
Karina Milanova <105791102+Karinski92@users.noreply.github.com>
2022-08-11 16:10:22 +0300

    Add files via upload

---
+++ ZooApp/src/main/java/zoo/entities/areas/LandArea.java
@@ -0,0 +1,10 @@
+package zoo.entities.areas;
+
+public class LandArea extends BaseArea{
+
+    private static final int CAPACITY = 10;
+
+    public LandArea(String name) {
+        super(name, CAPACITY);
+    }
+}
